c6f4e801d9c50e6659b2812bb5c7dc9c0f092d52
yang5864 <yang5864@naver.com>
2023-07-28 13:42:56 +0900

    JavaScript

---
+++ By Udemy/JavaScript/app.js
@@ -0,0 +1,28 @@
+// console.log("BEFORE CONDITIONAL")
+// // console.log("HELLO FROM OUT FIRST JS FILE!!!!");
+// // let total = 1 + 3;
+// // console.log("GOODBYE!");
+
+// let random = Math.random();
+
+// if (random < 0.5) {
+//     console.log("YOUR NUMBER IS LESS THAN 0.5!!!")
+//     console.log(random)
+// }
+// if (random >= 0.5) {
+//     console.log("YOUR NUMBER IS GREATER THAN 0.5!!!")
+//     console.log(random)
+// }
+// console.log("AFTER CONDITIONAL")
+
+const dayOfWeek = prompt('ENTER A DAY').toLowerCase();
+
+if (dayOfWeek === 'monday') {
+    console.log("UGHHH I HATE MONDAYS!")
+}
+else if (dayOfWeek === 'friday') {
+    console.log("우웹")
+}
+else {
+    console.log("우웩")
+}
---
+++ By Udemy/JavaScript/index.js
@@ -0,0 +1,8 @@
+let myLuckyNumber = 37;
+let octopusLimbs = 8;
+
+let bestColor = "purple"
+let quote = 'You had me at "hello"'
+
+const msg = "      TASTE THE RAINBOW!         ";
+let whisper = msg.trim().toLowerCase()
